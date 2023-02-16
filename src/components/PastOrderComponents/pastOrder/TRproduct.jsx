4fa67f8c726c2ce4b953397c085353123516257f
saurabhajn <118950949+saurabhsrb23@users.noreply.github.com>
2023-02-16 20:06:52 +0530

    Update TRproduct.jsx

--- src/components/PastOrderComponents/pastOrder/TRproduct.jsx
+++ src/components/PastOrderComponents/pastOrder/TRproduct.jsx
@@ -45,7 +45,7 @@ const TRproduct = ({product,key,user}) => {
               <tr className='pastOrdercontainerBody'>
               <td> {Orderid.substr(5,6) }</ td>
               < td> {DateP.substr(0,10)+" & "+t}</ td>
-              < td> Gp nagar</ td>
+              < td> Motihari</ td>
               < td> Motihari</ td>
               < td>{user.Phone ? user.Phone : '6204058598'}</ td>
               < td> {product.totalquantity}</ td>
